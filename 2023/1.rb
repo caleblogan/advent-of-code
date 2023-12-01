c0f3418f26ad61741a3db99f1a8c1ef8445d785b
Caleb Logan <clogan202@gmail.com>
2023-12-01 01:15:45 -0800

    2023 day 1 ruby

---
+++ 2023/1.rb
@@ -0,0 +1,42 @@
+def firstDigit(s)
+    for i in 0..s.length-1
+        if s[i] >= '1' && s[i] <= '9'
+            return s[i]
+        elsif s[i..i+2] == "one" or s[i..i+2] == "eno"
+            return '1'
+        elsif s[i..i+2] == "two" or s[i..i+2] == "owt"
+            return '2'
+        elsif s[i..i+4] == "three" or s[i..i+4] == "eerht"
+            return '3'
+        elsif s[i..i+3] == "four" or s[i..i+3] == "ruof"
+            return '4'
+        elsif s[i..i+3] == "five" or s[i..i+3] == "evif"
+            return '5'
+        elsif s[i..i+2] == "six" or s[i..i+2] == "xis"
+            return '6'
+        elsif s[i..i+4] == "seven" or s[i..i+4] == "neves"
+            return '7'
+        elsif s[i..i+4] == "eight" or s[i..i+4] == "thgie"
+            return '8'
+        elsif s[i..i+3] == "nine" or s[i..i+3] == "enin"
+            return '9'
+        end
+    end
+    return ""
+end
+
+if __FILE__ == $0
+    # 1abc2
+    # fiveabc6
+    file = File.open("input/1.txt", "r")
+    lines = file.readlines
+    sum = 0
+    lines.each { |l|
+        digits = ""
+        puts l
+        digits += firstDigit(l)
+        digits += firstDigit(l.reverse)
+        sum += digits.to_i
+    }
+    puts "the sum is #{sum}"
+end
