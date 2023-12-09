605f2dc73ed270eb5da0bffa0ba05b69f5f40a09
Caleb Logan <clogan202@gmail.com>
2023-12-08 21:31:13 -0800

    2023 9 a

---
+++ 2023/9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StreamReader sr = new(args[0]);
+        var res = 0L;
+        string? line;
+        Regex re = new Regex(@"(-?\d)+");
+        while ((line = sr.ReadLine()) != null)
+        {
+            var matches = re.Matches(line);
+            List<long> nums = new();
+            foreach (var m in matches)
+            {
+                nums.Add(long.Parse(m.ToString()));
+            }
+            res += NextNum(nums);
+        }
+        Console.WriteLine($"Res: {res}");
+
+    }
+
+    private static long NextNum(List<long> nums)
+    {
+        if (nums.All((long val) => val == 0))
+        {
+            return 0;
+        }
+        var newList = new List<long>();
+        for (int i = 1; i < nums.Count; i++)
+        {
+            newList.Add(nums[i] - nums[i - 1]);
+        }
+        return nums.Last() + NextNum(newList);
+    }
+}
