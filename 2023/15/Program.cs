b25ede639284fb8e3d5b70926a113169a7a8bdb1
Caleb Logan <clogan202@gmail.com>
2023-12-14 21:49:31 -0800

    2023 15 a

---
+++ 2023/15/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var sr = new StreamReader(args[0]);
+        long res = 0;
+        Console.WriteLine($"{Hash("qp=3")}");
+        foreach (var s in sr.ReadToEnd().Split(','))
+        {
+            res += Hash(s);
+        }
+
+        Console.WriteLine($"RES: {res}");
+    }
+    public static long Hash(string s)
+    {
+        long res = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            res += (byte)s[i];
+            res *= 17;
+            res %= 256;
+        }
+        return res;
+    }
+}
